b0cf55c3084e20964a44211f8159432d9f8107bd
chizidotdev <chizivwokoma@gmail.com>
2022-12-26 00:03:28 +0100

    working with sessions and cookies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/chizidotdev/golang-webapp/pkg/config"
 	"github.com/chizidotdev/golang-webapp/pkg/handlers"
 	"github.com/chizidotdev/golang-webapp/pkg/render"
@@ -12,8 +14,20 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+	// change to true when in prod
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -25,13 +25,21 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 
+// Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
+// About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, world."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
 		StringMap: stringMap,
